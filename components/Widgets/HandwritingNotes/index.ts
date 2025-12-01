9dbcf32379272e1fd8e0b92fae4c82b7915cf007
jfnukal <33260274+jfnukal@users.noreply.github.com>
2025-12-01 16:20:28 +0100

    Delete components directory

--- components/Widgets/HandwritingNotes/index.ts
+++
@@ -1,6 +0,0 @@
-// src/plugins/HandwritingNotes/index.ts
-export { default as HandwritingWidget } from './HandwritingWidget';
-export { default as CanvasDrawing } from './CanvasDrawing';
-export { ocrService } from './ocrService';
-export { notesStorage } from './notesStorage';
-export * from './types';
